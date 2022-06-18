128197abe372d56235bb6f90bd89397ab459d6fa
Shahid Kamal <shahidkamal08@gmail.com>
2022-06-19 00:30:12 +0530

    feat: remove local dns ip

--- bench-dns.sh
+++ bench-dns.sh
@@ -2,7 +2,7 @@ total_time=0
 count=1000
 for i in {1..1000}
 do
-	time=`dig @192.168.1.28 google.com | grep "Query time" | cut -d ' ' -f 4`
+	time=`dig google.com | grep "Query time" | cut -d ' ' -f 4`
 	total_time=$((total_time+time))
 #	sleep 1	
 done
